727fcf52e77580ee9c66c7d505a69916966da54f
mhong124 <mhong124@terpmail.umd.edu>
2024-06-19 17:38:25 -0400

    edited link redirection

--- PhotoWebsite/src/components/footer/Footer.tsx
+++ PhotoWebsite/src/components/footer/Footer.tsx
@@ -5,13 +5,13 @@ const Footer = () => {
     return(
         <div className = "footer">
             <div>
-                <a href = "https://mabelhong.com">Mabel Hong</a>
+                <a href = "https://mabelhong.com" target = "_blank">Mabel Hong</a>
                 <p>Thank you for viewing!</p>
             </div>
             <div> 
-                <a href = "https://www.instagram.com/mabeestory/"><img src = {getImageUrl("footer/insta.png")} height = "50" width = "50"></img></a>
-                <a href = "/"><img src = {getImageUrl("footer/lightroom.png")} height = "50" width = "50"></img></a>
-                <a href = "https://www.linkedin.com/in/mabel-hong"><img src = {getImageUrl("footer/linked.png")} height = "50" width = "50"></img></a>
+                <a href = "https://www.instagram.com/mabeestory/" target = "_blank"><img src = {getImageUrl("footer/insta.png")} height = "50" width = "50"></img></a>
+                <a href = "/" target = "_blank"><img src = {getImageUrl("footer/lightroom.png")} height = "50" width = "50"></img></a>
+                <a href = "https://www.linkedin.com/in/mabel-hong" target = "_blank"><img src = {getImageUrl("footer/linked.png")} height = "50" width = "50"></img></a>
             </div>
         </div>
     );
