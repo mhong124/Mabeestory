396e4d521fb4f7545afd7c3d2cd1db71e1c0a12f
mhong124 <mhong124@terpmail.umd.edu>
2024-06-20 22:08:19 -0400

    Update About.tsx

--- PhotoWebsite/src/components/about/About.tsx
+++ PhotoWebsite/src/components/about/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
                     <p>Hello! I am a beginner in photography, but I love to learn more about it! 
                         I have been exploring small digital cameras as well as my first DSLR. 
                         I love taking photos of things I find beautiful, delicious, and loved! Though I have only really shot 
-                        nature, flowers, and everything pretty in front of me, I hope to start learning the art of shooting
+                        nature, flowers, and everything pretty in front of me, I hope to start learning the art of photoshooting
                         people that I love! Follow me on my journey :D</p>
                 </div>
                 <div className = "img">
